f9bb12cbb7156d1d1066ccfadbf27a9fe8aad201
Bin_TestProject <45866008+bin-hw92@users.noreply.github.com>
2022-05-02 14:39:58 +0900

    서피스로 수정

--- src/components/calendar/CalendarView.js
+++ src/components/calendar/CalendarView.js
@@ -1,10 +1,16 @@
-
+import "../../css/Todo.css";
 
 const CalendarView = ({calendars}) => {
     return (
-        <div>{calendars.map(({_id, title}) => (
-            <div key={_id}>{title}</div>
-        ))}</div>
+        <div className="todo-list">
+            {calendars.map(({_id, title, startDate, endDate}) => (
+            <ul className="todo-list-item" key={_id}>
+                <li>{startDate.hour}.{startDate.min}</li>
+                <li>{title}</li>
+                <li></li>
+            </ul>
+            ))}
+        </div>
     );
 };
 
